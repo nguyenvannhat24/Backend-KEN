afd7a2d0b723ebccb45606af84c9f5224366560b
pkd11011 <phamdo1132@gmail.com>
2025-09-29 15:39:57 +0700

    Chuan hoa api

--- models/boardMember.model.js
+++ models/boardMember.model.js
@@ -1,22 +1,19 @@
   const mongoose = require('mongoose');
   const { Schema } = mongoose;
 
-  const BoardMemberSchema = new mongoose.Schema({
-
-    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
-    board_id: { type: Schema.Types.ObjectId, ref: 'Board', required: true, index: true },
-     role_in_board: { 
+const BoardMemberSchema = new mongoose.Schema({
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+  board_id: { type: Schema.Types.ObjectId, ref: 'Board', required: true, index: true },
+  role_in_board: { 
     type: String, 
-    enum:["Người tạo", "Thành viên", "Khách"], 
-    default: "Khách", 
-   
+    enum: ["Người tạo", "Thành viên", "Khách"], 
+    default: "Khách"
   },
   Creator: {
     type: Boolean,
     default: false
-  },
-    
-  }, { collection: 'BoardMembers', timestamps: true });
+  }
+}, { collection: 'BoardMembers', timestamps: true });
 
 
   module.exports = mongoose.model('BoardMember', BoardMemberSchema);

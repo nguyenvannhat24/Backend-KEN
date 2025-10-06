c4deb8cccc3da6b9ac034f9dae323b51e59ba4fa
nguyenvannhat24 <nhatx5xxx@gmail.com>
2025-10-06 08:05:40 +0700

    merge

---
+++ models/Permission.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PermissionSchema = new mongoose.Schema(
+  {
+    description: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+  },
+  { collection: 'Permissions', timestamps: true }
+);
+
+module.exports = mongoose.model('Permission', PermissionSchema);
